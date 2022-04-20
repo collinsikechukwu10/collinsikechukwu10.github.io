64e997851850b400209d552ee49f1cdead7d7d17
ikechukwu-ubuntu <iic1@st-andrews.ac.uk>
2022-04-20 04:41:27 +0100

    fixed all urls, fixed word cloud

--- js/main.js
+++ js/main.js
@@ -57,6 +57,58 @@ $(document).ready(function () {
     });
 
 
+    // contact submittion
+    $("#email-form .submit").on("click", function (event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var phonenumber = $("input#phonenumber").val();
+      var message = $("textarea#email-message").val();
+      // let submitButton = $(this);
+      // submitButton.prop("disabled", true); // Disable button until AJAX call is complete
+      // let submissionData = {name, phonenumber, email, message};
+      alert("Sorry " + name + ", it seems that my mail server is not responding. Please try again later!")
+      /**
+       * $.ajax({
+        url: "/mail",
+        type: "GET",
+        data:submissionData,
+        cache: false,
+        success: function (data) {
+          // Success message
+          console.log(data);
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          //clear all fields
+          $('#email-form').trigger("reset");
+        },
+        error: function () {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#email-form').trigger("reset");
+        },
+        complete: function () {
+          setTimeout(function () {
+            submitButton.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      });
+       * 
+       */
+      
+    });
+
   })(jQuery); // End of use strict
 }
 )
